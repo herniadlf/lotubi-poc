51d4378a950d21b182f58da8fb1ec89cbb8eb72e
Hernán de la Fuente <hernan.de@mercadolibre.com>
2022-04-10 17:37:35 -0300

    Add coverage to selected number

--- test/contracts/LotUbiTest.js
+++ test/contracts/LotUbiTest.js
@@ -14,10 +14,29 @@ describe('LotUbi', function () {
     });
 
     describe('Deployment', function () {
-
         it('Should initialize empty', async function () {
            expect(await lotUbiInstance.userChoice()).to.equal(ethers.constants.Zero);
            expect(await lotUbiInstance.userAddress()).to.equal(ethers.constants.AddressZero);
+        });
+    });
+
+    describe('Choose number', function () {
+        it('Should fail because an invalid number param', async function () {
+            [owner, addr1] = await ethers.getSigners();
+
+            try{
+                await lotUbiInstance.chooseNumber(-1);
+            } catch (err) {
+                expect(err).to.be.instanceOf(Error)
+            }
+        });
+
+        it('Should set selected number', async function () {
+            [owner, someUserAddress] = await ethers.getSigners();
+
+            await lotUbiInstance.connect(someUserAddress).chooseNumber(1);
+            expect(await lotUbiInstance.userChoice()).to.equal(1);
+            expect(await lotUbiInstance.userAddress()).to.equal(someUserAddress.address);
         });
     });
 
